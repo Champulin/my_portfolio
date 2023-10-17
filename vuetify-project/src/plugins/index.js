18ee642caf7a2f963a1206710f92d9d14a88060a
Champulin <Champustrike@gmail.com>
2023-10-17 19:46:40 +0200

    changes to deploy

--- vuetify-project/src/plugins/index.js
+++ vuetify-project/src/plugins/index.js
@@ -7,7 +7,8 @@
 // Plugins
 import vuetify from "./vuetify";
 import router from "../router";
-import { createI18n } from "vue-i18n";
+import { createI18n } from "vue-i18n/dist/vue-i18n.cjs.prod.js";
+//import { createI18n } from "vue-i18n";
 import messages from "@/locales";
 const i18n = createI18n({
   legacy: false,
